1614ddf8c6d74a93d9580a22f7d82df5f8aa3723
EVAPorator-sudo <osxar.jeavons@gmail.com>
2025-01-21 11:25:36 +0000

    added exit functionality

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from constants import *
 pygame.init()
 
 def main():
-	for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-        		return
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while(True):
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		for event in pygame.event.get():
+                	if event.type == pygame.QUIT:
+                        	return
 
 if __name__ == "__main__":
 	main()
