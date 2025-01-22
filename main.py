33ec31808f2de62275bf5c291b6ad9d71195eca3
EVAPorator-sudo <osxar.jeavons@gmail.com>
2025-01-22 14:50:55 +0000

    added player drawing functionality

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
-from constants import * 
+from constants import *
+from player import *
 pygame.init()
 
 def main():
+	Clock = pygame.time.Clock()
+	dt = 0
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 	while(True):
-		screen.fill((0,0,0))
-		pygame.display.flip()
 		for event in pygame.event.get():
-                	if event.type == pygame.QUIT:
-                        	return
+			if event.type == pygame.QUIT:
+				return
+		screen.fill((0,0,0))
+		player.draw(screen)
+		pygame.display.flip()
+		dt = Clock.tick(120) /1000
 
 if __name__ == "__main__":
 	main()
